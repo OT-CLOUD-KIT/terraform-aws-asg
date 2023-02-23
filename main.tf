5ad70d6973e799ce3d9d6ead4ab7d8968db4a416
KalraSanyam <126102245+KalraSanyam@users.noreply.github.com>
2023-02-23 12:22:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,95 @@
+# Launch Templates of Applications
+####launch Template Creation
+resource "aws_launch_template" "Application" {
+  name_prefix   = var.name_template
+  image_id      = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  
+  network_interfaces {
+    security_groups = [var.security_group]
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags          = var.server_name
+  }
+  tags = merge(
+    var.comman_tags,
+    var.template_tags
+  )
+}
+
+resource "aws_autoscaling_group" "Application" {
+  name = var.name
+  launch_template {
+    id      = aws_launch_template.Application.id
+    version = var.template_version
+  }
+  min_size            = var.min_size
+  max_size            = var.max_size
+  desired_capacity    = var.desired_size
+  vpc_zone_identifier = [var.vpc_zone_identifier_subnet, var.vpc_zone_identifier_subnet2]
+}
+
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "${var.name}-asg-scale-up"
+  policy_type            = var.policy_type_scale_up
+  adjustment_type        = var.adjustment_type
+  autoscaling_group_name = aws_autoscaling_group.Application.name
+  cooldown               = var.cooldown
+  scaling_adjustment     = var.scaling_up_adjustment
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
+  alarm_name          = "${var.name}-asg-scale-up-alarm"
+  alarm_description   = var.alarm_description 
+  comparison_operator = var.comparison_operator_scale_up
+  evaluation_periods  = var.scale_up_evaluation_periods
+  metric_name         = var.metric_name
+  namespace           = var.namespace
+  period              = var.period
+  statistic           = var.statistic
+  threshold           = var.scale_up_threshold
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.Application.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_up.arn]
+  tags = merge(
+    var.comman_tags,
+    var.cloudwatch_alarm_tags
+  )
+}
+
+# scale down policy
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "${var.name}-asg-scale-down"
+  autoscaling_group_name = aws_autoscaling_group.Application.name
+  adjustment_type        = var.adjustment_type
+  scaling_adjustment     = var.scale_down_adjustment
+  cooldown               = var.cooldown
+  policy_type            = var.policy_type_scale_down
+}
+
+# scale down alarm
+resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
+  alarm_name          = "${var.name}-asg-scale-down-alarm"
+  alarm_description   = var.alarm_description_scale_down
+  comparison_operator = var.comparison_operator_scale_down
+  evaluation_periods  = var.scale_down_evaluation_periods
+  metric_name         = var.metric_name
+  namespace           = var.namespace
+  period              = var.period
+  statistic           = var.statistic
+  threshold           = var.scale_down_threshold
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.Application.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
+  tags = merge(
+    var.comman_tags,
+    var.cloudwatch_alarm_tags
+  )
+}
