a9dc45079627454d1f576d79c67029503287e323
neelamkumariopstree <115863874+neelamkumariopstree@users.noreply.github.com>
2024-12-06 13:39:02 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,6 +2,12 @@
 variable "target_group_arns" {
   description = "Set of aws_alb_target_group ARNs for use with Application or Network Load Balancing."
 }
+variable "launch_template_version" {
+  description = "Launch template version. Can be version number, `$Latest`, or `$Default`"
+  type        = string
+  default     = null
+}
+
 variable "min_size" {
   type        = number
   default     = 1
